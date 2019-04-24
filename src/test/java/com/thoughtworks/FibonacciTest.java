445ec7d75a9ce12aaf38eac5c459708d6c20a273
Liu Lu <yacht2005@gmail.com>
2019-04-24 20:23:19 +0800

    setup test environment

---
+++ src/test/java/com/thoughtworks/FibonacciTest.java
@@ -0,0 +1,11 @@
+package com.thoughtworks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTest {
+    @Test
+    public void setupShouldBeReady(){
+        Assert.assertEquals(0, 0);
+    }
+}
