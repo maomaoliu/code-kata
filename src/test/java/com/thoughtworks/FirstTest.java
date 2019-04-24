fabe820ff8f1fe720cfaf3f004ee307166cf8f6a
Liu Lu <yacht2005@gmail.com>
2019-04-24 20:23:19 +0800

    setup test environment

---
+++ src/test/java/com/thoughtworks/FirstTest.java
@@ -0,0 +1,11 @@
+package com.thoughtworks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void setupShouldBeReady(){
+        Assert.assertEquals(0, 0);
+    }
+}
